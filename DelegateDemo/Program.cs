1d8eed7952ddd31316f2872e44c3ce7f21648728
anishadak0025 <anishadak4210@gmail.com>
2026-06-13 11:46:14 +0530

    delegate concepts with problem solving

---
+++ DelegateDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public delegate void MyDelegate();
+
+class Student
+{
+    public void Study()
+    {
+        Console.WriteLine("Student is studying");
+    }
+
+    public void Sleep()
+    {
+        Console.WriteLine("Student is sleeping");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student s = new Student();
+
+        MyDelegate activity;
+
+        activity = s.Study;
+
+        activity += s.Sleep;
+
+        activity();
+    }
+}
