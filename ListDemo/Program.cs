d51609ce5ea93f5d7ba7b134b269df5da33532bc
anishadak0025 <anishadak4210@gmail.com>
2026-06-22 13:22:37 +0530

    list and Add Remove Fuction

---
+++ ListDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+      List<int> students = new List<int>()
+        {
+            1,
+            2
+        };
+    
+       foreach (int s in students)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
+
